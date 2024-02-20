e3fed259e82ede869fcda2a5ec5dec1ebe6f6056
moodyUtsubo <157574749+moodyUtsubo@users.noreply.github.com>
2024-02-20 09:59:35 +0700

    Add files via upload

--- auth/jwt.py
+++ auth/jwt.py
@@ -1,13 +1,16 @@
 from datetime import datetime, timedelta, timezone
 from typing import Union
 
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from typing_extensions import Annotated
 
-from .userdb import fake_users_db, Token, TokenData, User, UserInDB
+from .token import TokenData
+from data import models, schemas
+from data.database import SessionLocal
+from sqlalchemy.orm import Session
 
 
 # to get a string like this run:
@@ -20,7 +23,13 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 def verify_password(plain_password, hashed_password):
@@ -31,14 +40,12 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
+def get_user(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
 
 
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
+def authenticate_user(db, username: str, password: str):
+    user = get_user(db, username)
     if not user:
         return False
     if not verify_password(password, user.hashed_password):
@@ -57,7 +64,7 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     return encoded_jwt
 
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -71,14 +78,14 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
+    user = get_user(db, username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
 
 
 async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)]
+    current_user: Annotated[schemas.User, Depends(get_current_user)]
 ):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
@@ -86,7 +93,7 @@ async def get_current_active_user(
 
 
 async def get_current_active_admin(
-    current_admin: Annotated[User, Depends(get_current_active_user)]
+    current_admin: Annotated[schemas.User, Depends(get_current_active_user)]
 ):
     if current_admin.role != "admin":
         raise HTTPException(status_code=400, detail="Inactive admin")
