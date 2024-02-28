ea3897e25722390206f0c0d1c5f5b7daabda6e82
moodyUtsubo <157574749+moodyUtsubo@users.noreply.github.com>
2024-02-28 11:14:59 +0700

    Update movies.py

--- routers/movies.py
+++ routers/movies.py
@@ -4,82 +4,60 @@ from typing import Union, List
 from typing_extensions import Annotated
 
 from sqlalchemy.orm import Session
-from data.database import SessionLocal
 
-from auth.jwt import get_current_active_user, get_current_active_admin, get_db
+from auth.jwt import get_current_active_user, get_current_active_admin
 
-from data import crud, models, schemas
+from data.models import movie_models
+from data.schemas import movie_schemas, user_schemas
+from data.crud import movie_crud
+
+from postgres_session.database import get_db
 
 
 router = APIRouter()
 
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
+
+@router.post("/api/v1/movies/", response_model=movie_schemas.Movie, tags=["movies"])
+def create_movie(_: Annotated[user_schemas.User, Depends(get_current_active_admin)], 
+                 movie: movie_schemas.MovieCreate, db: Session = Depends(get_db)):
+    return movie_crud.create_movies(db=db, movie=movie)
 
 
-@router.post("/api/v1/movies/", response_model=schemas.Movie, tags=["movies"])
-def create_movie(current_admin: Annotated[schemas.User, Depends(get_current_active_admin)], 
-                 name: str, type: models.Type, genres: List[models.Genre] = Query(...), db: Session = Depends(get_db)):
-    movie = models.Movie(name=name, type=type, genres=genres)
-    # db_user = crud.get_movies(db, email=user.email)
-    # if db_user:
-    #     raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_movies(db=db, movie=movie)
-
-
-@router.get("/api/v1/movies/", response_model=List[schemas.Movie], tags=["movies"])
-def read_movies(current_user: Annotated[schemas.User, Depends(get_current_active_user)], 
+@router.get("/api/v1/movies/", response_model=List[movie_schemas.Movie], tags=["movies"])
+def read_movies(_: Annotated[user_schemas.User, Depends(get_current_active_user)], 
                 name: Annotated[Union[str, None], Query(max_length=100)] = None, 
-                type: models.Type = None, 
-                genres: List[models.Genre] = Query(None), 
+                type: movie_models.Type = None, 
+                genres: List[movie_models.Genre] = Query(None), 
                 skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    movies = db.query(movie_models.Movie)
     if name:
-        movies = crud.get_movies_by_name(db, name=name)
-        if movies != []:
-            return movies
-        raise HTTPException(status_code=404, detail=f"Failed, movie {name} not found!")
-    elif type:
-        movies = crud.get_movies_by_type(db, type=type)
-        if movies != []:
-            return movies
-        raise HTTPException(status_code=404, detail=f"Failed, movie with type {type} not found!")
-    elif genres:
-        movies = crud.get_movies_by_genre(db, genre=genres)
-        if movies != []:
-            return movies
-        raise HTTPException(status_code=404, detail=f"Failed, movie with genres {genres} not found!")
-    else:
-        movies = crud.get_movies(db, skip=skip, limit=limit)
+        movies = movies.filter(movie_models.Movie.name.contains(name))
+    if type:
+        movies = movies.filter(movie_models.Movie.type == type)
+    if genres:
+        movies = movies.filter(movie_models.Movie.genres.contains(genres))
+    movies = movies.offset(skip).limit(limit).all()
+    if movies:
         return movies
+    raise HTTPException(status_code=404, detail=f"Failed, movies with input queries not found!")
 
 
-@router.put("/api/v1/movies/{id}", response_model=schemas.Movie, tags=["movies"])
-def update_movie(current_user: Annotated[schemas.User, Depends(get_current_active_admin)], id: int,
-                name: Annotated[Union[str, None], Query(max_length=100)] = None, 
-                type: models.Type = None, 
-                genres: List[models.Genre] = Query(None), 
+@router.patch("/api/v1/movies/{id}", response_model=movie_schemas.Movie, tags=["movies"])
+def update_movie(_: Annotated[user_schemas.User, Depends(get_current_active_admin)], id: int,
+                upd_movie: movie_schemas.MovieUpdate, 
                 db: Session = Depends(get_db)):
-    movie = crud.get_movie(db, id=id)
-    if movie:
-        if name:
-            crud.update_movies_name(db, id=id, name=name)
-        if type:
-            crud.update_movies_type(db, id=id, type=type)
-        if genres:
-            crud.update_movies_genres(db, id=id, genre=genres)
-        movie = crud.get_movie(db, id=id)
-        return movie
-    raise HTTPException(status_code=404, detail=f"Failed, movie with id {id} not found!")
+    movie = movie_crud.get_movie(db, id=id)
+    if movie is None:
+        raise HTTPException(status_code=404, detail=f"Failed, movie with id {id} not found!")
+    movie_crud.update_movie(db, movie=movie, upd_movie=upd_movie)
+    movie = movie_crud.get_movie(db, id=id)
+    return movie
 
 
-@router.delete("/api/v1/movies/{id}", response_model=List[schemas.Movie], tags=["movies"])
-def delete_movie(current_user: Annotated[schemas.User, Depends(get_current_active_admin)], id: int, db: Session = Depends(get_db)):
-    movie = crud.get_movie(db, id=id)
+@router.delete("/api/v1/movies/{id}", response_model=List[movie_schemas.Movie], tags=["movies"])
+def delete_movie(_: Annotated[user_schemas.User, Depends(get_current_active_admin)], id: int, db: Session = Depends(get_db)):
+    movie = movie_crud.get_movie(db, id=id)
     if movie:
-        db_movie = crud.delete_movie(db, id=id)
+        db_movie = movie_crud.delete_movie(db, id=id)
         return db_movie
     raise HTTPException(status_code=404, detail=f"Failed, movie with id {id} not found!")
