2892618e8369c6af5173489a597b87762ce02e46
moodyUtsubo <157574749+moodyUtsubo@users.noreply.github.com>
2024-02-28 11:15:47 +0700

    Update users.py

--- routers/users.py
+++ routers/users.py
@@ -8,8 +8,11 @@ from fastapi.security import OAuth2PasswordRequestForm
 from typing_extensions import Annotated
 
 from auth.token import Token
-from data import schemas, crud, models
-from auth.jwt import authenticate_user, create_access_token, get_current_active_user, get_db, get_password_hash, get_current_active_admin
+from data.models import user_models
+from data.schemas import user_schemas
+from data.crud import user_crud
+from auth.jwt import authenticate_user, create_access_token, get_current_active_user, get_current_active_admin
+from postgres_session.database import get_db
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -34,34 +37,32 @@ async def login_for_access_token(
     return Token(access_token=access_token, token_type="bearer")
 
 
-@router.get("/users/me/", response_model=schemas.User, tags=["user"])
+@router.get("/users/me/", response_model=user_schemas.User, tags=["user"])
 async def read_users_me(
-    current_user: Annotated[schemas.User, Depends(get_current_active_user)]
+    current_user: Annotated[user_schemas.User, Depends(get_current_active_user)]
 ):
     return current_user
 
 
-@router.post("/signup/", response_model=schemas.User, tags=["user"])
-async def signup(full_name: str, email: str, username: str, password: str, 
+@router.post("/signup/", response_model=user_schemas.User, tags=["user"])
+async def signup(user: user_schemas.UserCreate, 
                  db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=email)
+    db_user = user_crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    db_user = crud.get_user_by_username(db, username=username)
+    db_user = user_crud.get_user_by_username(db, username=user.username)
     if db_user:
         raise HTTPException(status_code=400, detail="Username already existed")
-    hashed_password = get_password_hash(password)
-    user = models.User(full_name=full_name, email=email, username=username, hashed_password=hashed_password)
-    return crud.create_user(db=db, user=user)
+    return user_crud.create_user(db=db, user=user)
 
 
-@router.put("/role/{id}", response_model=schemas.User, tags=["user"])
-def role(current_admin: Annotated[schemas.User, Depends(get_current_active_admin)], 
-               id: int, role: models.Role, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_id(db, id=id)
+@router.put("/role/{id}", response_model=user_schemas.User, tags=["user"])
+def role(_: Annotated[user_schemas.User, Depends(get_current_active_admin)], 
+               id: int, role: user_models.Role, db: Session = Depends(get_db)):
+    db_user = user_crud.get_user_by_id(db, id=id)
     if db_user:
         # if role:
-        crud.user_role(db, id=id, role=role)
-        db_user = crud.get_user_by_id(db, id=id)
+        user_crud.user_role(db, id=id, role=role)
+        db_user = user_crud.get_user_by_id(db, id=id)
         return db_user
     raise HTTPException(status_code=404, detail=f'User with id {id} not found')
